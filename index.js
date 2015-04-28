9da2357e2b44caea4c41e7c85d07d6d0300a8d02
Joe Hudson <joehud@gmail.com>
2015-04-27 21:36:07 -0400

    marker 2

---
+++ index.js
@@ -0,0 +1,8 @@
+var engine = require('./js/engine');
+
+module.exports = {
+  reset: engine.reset,
+  startBook: engine.startBook,
+  currentPage: engine.currentPage,
+  showPage: engine.showPage
+};
